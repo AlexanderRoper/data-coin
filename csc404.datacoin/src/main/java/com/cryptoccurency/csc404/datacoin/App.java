50a251335768e8abdee8065e1d8ccda6e4ed7194
Xander <roper.alexander@gmail.com>
2022-10-31 22:03:36 -0400

    Add wallet

--- csc404.datacoin/src/main/java/com/cryptoccurency/csc404/datacoin/App.java
+++ csc404.datacoin/src/main/java/com/cryptoccurency/csc404/datacoin/App.java
@@ -1,13 +1,30 @@
-package com.cryptoccurency.csc404.datacoin;
+package com.cryptoccurency.csc404.datacoin.util;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import java.util.ArrayList;
+
+public interface Wallet {
+    /*
+     * Wallet class
+     */
+
+    // Amount in Wallet
+    public void getSeedBalance();
+
+    // Get transaction history
+    public ArrayList<String> getPreviousTransactions();
+
+    // Create 256-bit keys
+    public String generatePrivateKey();
+
+    public String generatePrivateKey();
+
+    // Store 256-bit keys
+    public void storePrivateKey();
+
+    public void storePublicKey();
+
+    // Combined seed words for wallet access
+    public void setSeedPhrase(String[] phrases);
+
+    public void checkSeedPhrase(String[] phrases);
 }
